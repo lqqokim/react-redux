bf7968e1d658599459de903e4c7d56fe511eadd0
lqqokim <kis420404@gmail.com>
2020-04-03 01:05:15 +0900

    feat: add routes

---
+++ src/routes/Home.js
@@ -0,0 +1,14 @@
+export default () => "Home";
+// import React from 'react';
+
+// function Home() {
+//     return (
+//         <>
+//             <h1>To Do</h1>
+//             <form>
+//                 <input type="text" value={text} />
+//             </form>
+//             <ul></ul>
+//         </>
+//     )
+// }
